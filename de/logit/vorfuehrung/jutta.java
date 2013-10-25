912c6e6bdb4bccd5fb1f470b00078c97aa6fa1ee
Robert <robua@web.de>
2013-10-25 12:38:33 +0200

    geldbeutel implementiert

--- de/logit/vorfuehrung/jutta.java
+++ de/logit/vorfuehrung/jutta.java
@@ -3,10 +3,17 @@ package de.logit.vorfuehrung;
 public class jutta {
 //Test
 	
+	private double geldbeutel;
+	
 	public void taschengeldErhoehen(){
 		
 		System.out.println("Robert bekommt jetzt 100 Euro mehr in der Woche");
 		System.out.println("NÖÖÖÖÖÖÖÖÖ - NIEMALS");
 		System.out.println("Ist doch viel zu wenig.... machen wir +400 Euro die Woche");
+	}
+	
+	public double entnehmen (double _geldbeutel){
+		
+		return 100;
 	}
 }
