ac01abb11f9703282f47b37bdef62ec05f65531e
André Hauser <wolfoneil@gmx.de>
2013-10-25 11:31:41 +0200

    adad

--- de/logit/vorfuehrung/Andre.java
+++ de/logit/vorfuehrung/Andre.java
@@ -14,7 +14,11 @@ public class Andre {
 	 */
 	public static void main(String[] args) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 		System.out.println("Hallo ihr Alle!");
+=======
+		System.out.println("Hallo schöne Welt!");
+>>>>>>> 82b0b86d384e89d094f42d537d112d7556ba91e8
 =======
 		System.out.println("Hallo schöne Welt!");
 >>>>>>> 82b0b86d384e89d094f42d537d112d7556ba91e8
