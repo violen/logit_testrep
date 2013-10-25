e71d8085e18a4acb6ac72004de02209ba1d1add5
Robert <robua@web.de>
2013-10-25 12:29:17 +0200

    TG Er.

--- de/logit/vorfuehrung/jutta.java
+++ de/logit/vorfuehrung/jutta.java
@@ -2,4 +2,9 @@ package de.logit.vorfuehrung;
 
 public class jutta {
 //Test
+	
+	public void taschengeldErhoehen(){
+		
+		System.out.println("Robert bekommt jetzt 100 Euro mehr in der Woche");
+	}
 }
