a4a7fc19f5ba66fc626576751ab0d6b04bc3443a
André Hauser <wolfoneil@gmx.de>
2013-10-25 11:52:05 +0200

    Nu aber Korrekt !

--- de/logit/vorfuehrung/Andre.java
+++ de/logit/vorfuehrung/Andre.java
@@ -13,21 +13,8 @@ public class Andre {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 		System.out.println("Hallo ihr Alle!");
-
-=======
-<<<<<<< HEAD
-		System.out.println("Hallo ihr Alle!");
-=======
-		System.out.println("Hallo schöne Welt!");
->>>>>>> 82b0b86d384e89d094f42d537d112d7556ba91e8
->>>>>>> parent of db55054... korrekt
-=======
-		System.out.println("Hallo schöne Welt!");
->>>>>>> 82b0b86d384e89d094f42d537d112d7556ba91e8
 
 	}
 
